9d636c31d8f963e2defa34272a9deb8e90b29f22
Cybi1 <derenok_ilya@mail.ru>
2023-02-26 22:26:13 +0300

    Beautify and refactor

--- cybi.js
+++ cybi.js
@@ -1,37 +1,37 @@
-const PAPER = "paper"
-const ROCK = "rock"
-const SCISSORS = "scissors"
-let choice = [PAPER, ROCK, SCISSORS];
+const PAPER = "paper";
+const ROCK = "rock";
+const SCISSORS = "scissors";
+const CHOICES = [PAPER, ROCK, SCISSORS];
 
-function getComputerChoice () {
-    const randomValue = Math.floor(Math.random() * choice.length);
-    return choice[randomValue];
+function getComputerChoice() {
+  const randomValue = Math.floor(Math.random() * CHOICES.length);
+  return CHOICES[randomValue];
 }
 
+function gethumanChoice() {
+  return prompt("your choice:");
+}
 
- function humanChoice() {
-    return prompt("your choice:");
- }
+function humanWin(computerValue, humanValue) {
+  return (
+  (computerValue === SCISSORS && humanValue === ROCK) ||
+  (computerValue === ROCK && humanValue === PAPER) ||
+  (computerValue === PAPER && humanValue === SCISSORS)
+  );
+}
 
+function play(computerValue, humanValue) {
+  if (computerValue === humanValue) return("draw");
+  if (humanWin(computerValue, humanValue)) return ("win");
 
+  return ("lose");
+}
 
- function play(computerValue, humanValue) {
-     if (computerValue === humanValue) 
-         return("draw");
-     else if ((computerValue === SCISSORS && humanValue === ROCK) ||
-        (computerValue === ROCK && humanValue === PAPER) ||
-        (computerValue === PAPER && humanValue === SCISSORS)) {
-        return ("win");
-        }
-    else
-        return ("lose");
- }
-
- while(true) {
-let computerValue = getComputerChoice();
-let humanValue = humanChoice();
-let result = play(computerValue,humanValue);
-if (humanValue === "exit") break;
-console.log(`Your choice:${humanValue}. Computer choice:${computerValue}.`)
-console.log(result);
+while(true) {
+  let computerValue = getComputerChoice();
+  let humanValue = gethumanChoice();
+  let result = play(computerValue, humanValue);
+  if (humanValue === "exit") break;
+  console.log(`Your choice: ${humanValue}. Computer choice: ${computerValue}.`);
+  console.log(result);
 }
